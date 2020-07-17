c554fb12e551af50fb219d66a6abdeca1e099238
Gustaf Rosenblad <gustaf@ccode.se>
2020-07-17 11:01:08 +0200

    Initial action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Not implemented"
+# Copy files into place
+mkdir -p "$GOPATH/src/$PACKAGES_PREFIX/"
+for PACKAGE in $(echo "$PACKAGES" | sed "s/,/ /g"); do
+    echo "Copying $PACKAGE..."; 
+    cp -R "$PACKAGE" "$GOPATH/src/$PACKAGES_PREFIX/$PACKAGE";
+done
+cp main.go "$GOPATH/src/$PACKAGES_PREFIX/main.go" || true
+
+# Run tests and copy coverage
+mkdir coverprofiles/
+PACKAGE_TOTALS=""
+for PACKAGE in $(echo "$TEST_PACKAGES" | sed "s/,/ /g"); do
+    echo "Testing $PACKAGES_PREFIX/$PACKAGE..."; 
+    go test -timeout 300s -parallel 1 -v "-coverprofile=coverprofiles/${PACKAGE/\//-}" "$PACKAGES_PREFIX/$PACKAGE" | tee "$PACKAGE.log";
+    PACKAGE_TOTAL=$(cat "$PACKAGE.log" | grep coverage | grep -o '[^ ]*%');
+    PACKAGE_TOTAL="${PACKAGE_TOTAL//[!0-9.]/}";
+    PACKAGE_TOTALS=$(printf "$PACKAGE_TOTALS\n$PACKAGE_TOTAL");
+done
+
+# Print coverage
+echo "$PACKAGE_TOTALS" | tail -n +2 > totals
+AVG=$(awk '{ total += $0; count++ } END { print total/count }' totals)
+"echo 'Code Coverage: '$AVG'%'"
