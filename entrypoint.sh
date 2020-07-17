ed9260bc5c279882e315d04bd049d4e904ec21df
Gustaf Rosenblad <gustaf@ccode.se>
2020-07-17 11:01:08 +0200

    Initial action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Not implemented"
+mkdir -p "$GOPATH/src/$PACKAGES_PREFIX/"
+for PACKAGE in $(echo "$PACKAGES" | sed "s/,/ /g"); do
+    echo "Copying $PACKAGE..."; 
+    cp -R "$PACKAGE" "$GOPATH/src/$PACKAGES_PREFIX/$PACKAGE";
+done
+
+cp main.go "$GOPATH/src/$PACKAGES_PREFIX/main.go" || true
+
+for PACKAGE in $(echo "$PACKAGES" | sed "s/,/ /g"); do
+    $GOPATH/bin/golint -set_exit_status $PACKAGE;
+done
